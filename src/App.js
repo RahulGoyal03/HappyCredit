0d6e286c9ba1d91904be7af398af5f699bf2151c
RahulGoyal03 <91531231+RahulGoyal03@users.noreply.github.com>
2022-03-17 23:21:59 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
-import { Navbar } from "./Components/Navbar.jsx";
-import { Heading } from "./Components/Heading.jsx";
+// import { Navbar } from "./Components/Navbar.jsx";
+// import { Heading } from "./Components/Heading.jsx";
+// import { Category } from "./Components/Category.jsx";
+import { Card } from "./Components/Card.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Heading />
+      {/* <Navbar /> */}
+      {/* <Heading /> */}
+      {/* <Category /> */}
+      <Card />
     </div>
   );
 }
